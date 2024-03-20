cacdb9a86626a78b8526da22fa58a190cbedaefc
Matt Sárdi <sardi.matt@gmail.com>
2024-03-20 23:53:32 +0100

    feat(cv): adjust local cv example to work, add dependencies

--- python-backend/food_vision_backend/local_computer_vision/example.py
+++ python-backend/food_vision_backend/local_computer_vision/example.py
@@ -1,19 +1,17 @@
+import matplotlib.pyplot as plt
 import numpy as np
-import skimage
 from PIL import Image, ImageDraw
+from pyprojroot import here
 from transformers import pipeline
 
 checkpoint = "google/owlv2-base-patch16-ensemble"
 detector = pipeline(model=checkpoint, task="zero-shot-object-detection")
 
-image = skimage.data.astronaut()
-image = Image.fromarray(np.uint8(image)).convert("RGB")
-
-image
+image = Image.open(here("../data/example-images/big-mac.jpg"))
 
 predictions = detector(
     image,
-    candidate_labels=["human face", "rocket", "nasa badge", "star-spangled banner"],
+    candidate_labels=["food", "drink"],
 )
 
 draw = ImageDraw.Draw(image)
@@ -27,4 +25,5 @@ for prediction in predictions:
     draw.rectangle((xmin, ymin, xmax, ymax), outline="red", width=1)
     draw.text((xmin, ymin), f"{label}: {round(score,2)}", fill="white")
 
-image
+plt.imshow(np.asarray(image))
+plt.show()
